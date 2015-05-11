151c6e77e9dd306d9280a1640702230a9e4d57ee
z3nRob <rhink@zendesk.com>
2015-05-11 13:24:41 +0100

    get requester id
    commented code is for a request that gets the requested tickets, but
    for some reason not working yet.

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 (function() {
-
-  return {
-    events: {
-      'app.activated':'doSomething'
-    },
-
-    doSomething: function() {
-    }
-  };
-
+    return {
+        /*requests: {
+            getTotalTickets: function(requester_id){
+                url: '/api/v2/users/' + requester_id + '/tickets/requested.json',
+                type: 'GET',
+                dataType: 'json'
+            }
+        },*/
+        
+        events: {
+            'app.activated': 'getRequesterData'            
+        },
+        
+        getRequesterData: function() {
+            var requester_id = this.ticket().requester().id();
+            /*this.ajax('getTotalTickets', requester_id)
+                .done(function(data) {
+                    console.dir(data);
+            });*/
+        }
+        
+    };
 }());
