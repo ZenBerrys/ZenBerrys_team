5b4ddaf2208239ff7f6d7fe0c995c73901d438f0
z3nRob <rhink@zendesk.com>
2015-05-11 13:44:50 +0100

    updated with requester info + to-do
    we can now look for tickets made by the requester
    I added some comments with what to do next:
    
    Look for satisfaction ratings per ticket
    Look for tickets made by channel.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 (function() {
     return {
-        /*requests: {
+        requests: { 
             getTotalTickets: function(requester_id){
-                url: '/api/v2/users/' + requester_id + '/tickets/requested.json',
-                type: 'GET',
-                dataType: 'json'
+                return {
+                    url: "/api/v2/users/" + requester_id + "/tickets/requested.json",
+                    type: "GET",
+                    dataType: "json"
+                }
             }
-        },*/
+        },
         
         events: {
             'app.activated': 'getRequesterData'            
@@ -14,11 +16,14 @@
         
         getRequesterData: function() {
             var requester_id = this.ticket().requester().id();
-            /*this.ajax('getTotalTickets', requester_id)
+            this.ajax('getTotalTickets', requester_id)
                 .done(function(data) {
-                    console.dir(data);
-            });*/
+                    //capture capture satisfactions
+                    //capture tickets by channels
+                    //fill in the details inside the app
+                    this.$("#zTotal")[0].innerHTML = data.count; //fill in total count
+                    
+            });
         }
-        
     };
 }());
